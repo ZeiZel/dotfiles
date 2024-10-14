4f953c3baac3eb9acad26fb7d1095d5e3a266338
ZeiZel <valera2003lvov@yandex.ru>
2024-10-14 17:11:53 +0300

    feat(ui): added "show usages"

--- nv/lua/plugins/conform.lua
+++ nv/lua/plugins/conform.lua
@@ -19,7 +19,7 @@ return {
 				format_on_save = {
 					lsp_fallback = true,
 					async = false,
-					timeout_ms = 1000,
+					timeout_ms = 2000,
 				},
 			})
 			vim.api.nvim_create_autocmd("BufWritePre", {
