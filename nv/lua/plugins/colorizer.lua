8291778119d0d1dc62211680ffa90fd45665c6fb
ZeiZel <valera2003lvov@yandex.ru>
2024-10-15 19:29:12 +0300

    feat(ui): prettified theme
    Signed-off-by: ZeiZel <valera2003lvov@yandex.ru>

--- nv/lua/plugins/colorizer.lua
+++ nv/lua/plugins/colorizer.lua
@@ -3,9 +3,12 @@ return {
 		"norcalli/nvim-colorizer.lua",
 		config = function()
 			require("colorizer").setup({
+				"html",
 				"css",
 				"javascript",
-				"html",
+				"typescript",
+				"typescriptreact",
+				"javascriptreact",
 				"lua",
 			})
 		end,
--- nv/lua/plugins/nvim-lint.lua
+++ nv/lua/plugins/nvim-lint.lua
@@ -7,12 +7,14 @@ return {
 			local lint = require("lint")
 
 			lint.linters_by_ft = {
-				typescript = { "eslint" },
-				typescriptreact = { "eslint" },
-				javascript = { "eslint" },
-				javascriptreact = { "eslint" },
+				typescript = { "eslint_d" },
+				typescriptreact = { "eslint_d" },
+				javascript = { "eslint_d" },
+				javascriptreact = { "eslint_d" },
 				svelte = { "eslint_d" },
 				python = { "pylint" },
+				css = { "stylelint" },
+				scss = { "stylelint" },
 			}
 
 			local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
