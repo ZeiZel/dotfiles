55e786297c1c3eda6dae74b7f80e53db53e3b498
ZeiZel <valera2003lvov@yandex.ru>
2024-10-15 20:00:17 +0300

    feat(dev): updated autosave / edited lsp
    Signed-off-by: ZeiZel <valera2003lvov@yandex.ru>

--- nv/lua/plugins/auto-save.lua
+++ nv/lua/plugins/auto-save.lua
@@ -1,23 +1,29 @@
 -- autosave files
+
+local group = vim.api.nvim_create_augroup("autosave", {})
+
+vim.api.nvim_create_autocmd("User", {
+	pattern = "AutoSaveWritePost",
+	group = group,
+	callback = function(opts)
+		if opts.data.saved_buffer ~= nil then
+			local filename = vim.api.nvim_buf_get_name(opts.data.saved_buffer)
+			print("AutoSave: saved " .. filename .. " at " .. vim.fn.strftime("%H:%M:%S"))
+		end
+	end,
+})
+
 return {
 	{
 		"okuuva/auto-save.nvim",
-		cmd = "ASToggle",                   -- optional for lazy loading on command
+		cmd = "ASToggle", -- optional for lazy loading on command
 		event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
 		opts = {
-			enabled = true,                 -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-			execution_message = {
-				enabled = true,
-				message = function() -- message to print on save
-					return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-				end,
-				dim = 0.18,                        -- dim the color of `message`
-				cleaning_interval = 1250,          -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-			},
-			trigger_events = {                     -- See :h events
+			enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
+			trigger_events = { -- See :h events
 				immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
 				defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-				cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
+				cancel_deferred_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
 			},
 			condition = nil,
 			write_all_buffers = true, -- write all buffers when the current one meets `condition`
