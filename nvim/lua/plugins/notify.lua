a55f9c731d8aa63a41525ccc3bc6b309ee9014ab
ZeiZel <valera2003lvov@yandex.ru>
2024-10-31 21:16:06 +0300

    fix(notify): maybe fixed devicon helper
    Signed-off-by: ZeiZel <valera2003lvov@yandex.ru>

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -4,7 +4,9 @@ return {
 		config = function()
 			local notifier = require("notify")
 			notifier.setup({
-				render = "default",
+				render = "compact",
+				max_height = 20,
+				max_width = 80,
 				top_down = false,
 				animation = "slide",
 				fps = 60,
