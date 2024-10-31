041cc372f9b6217304e173ffb16bbc6f45e6db79
ZeiZel <valera2003lvov@yandex.ru>
2024-10-31 21:16:06 +0300

    fix(notify): maybe fixed devicon helper
    Signed-off-by: ZeiZel <valera2003lvov@yandex.ru>

--- nvim/lua/core/configs.lua
+++ nvim/lua/core/configs.lua
@@ -1,30 +1,62 @@
--- Line numbers
-vim.wo.number = true
-vim.wo.relativenumber = true
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = false -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
+
+opt.wrap = false
 
 -- Mouse
-vim.opt.mouse = "a"
-vim.opt.mousefocus = true
+opt.mouse = "a"
+opt.mousefocus = true
 
--- Clipboard
-vim.opt.clipboard = "unnamedplus"
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+opt.cursorline = true
+
+-- turn on termguicolors for tokyonight colorscheme to work
+opt.termguicolors = true
+opt.background = "dark" -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+
+-- clipboard
+opt.clipboard = "unnamedplus"
+
+-- split windows
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
+
+-- turn off swapfile
+opt.swapfile = false
 
 -- Tabs
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
 
 -- Other
-vim.opt.scrolloff = 8
-vim.opt.wrap = false
-vim.opt.termguicolors = true
+opt.scrolloff = 8
+opt.wrap = false
+opt.termguicolors = true
 
 -- Fillchars
-vim.opt.fillchars = {
+opt.fillchars = {
 	vert = "│",
 	fold = "⠀",
 	eob = " ", -- suppress ~ at EndOfBuffer
-	-- diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
+	diff = "⣿", -- alternatives = ⣿ ░ ─ ╱
 	msgsep = "‾",
 	foldopen = "▾",
 	foldsep = "│",
