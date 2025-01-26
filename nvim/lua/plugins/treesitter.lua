344902b59bf4785618625455a9b6a019c00cc051
ZeiZel <valera2003lvov@yandex.ru>
2025-01-26 11:09:28 +0300

    feat(nvims): added few configs
    Signed-off-by: ZeiZel <valera2003lvov@yandex.ru>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,61 +1,15 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				incremental_selection = {
-					enable = true,
-					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
-					},
-				},
-				auto_install = true,
-				highlight = {
-					enable = true,
-				},
-				indent = { enable = true },
-				ensure_installed = {
-					-- Lua & Vim
-					"lua",
-					"vim",
-					"vimdoc",
-
-					-- JS/TS related
-					"tsx",
-					"typescript",
-					"javascript",
-					"astro",
-					"svelte",
-
-					-- Web / App related
-					"html",
-					"css",
-					"graphql",
-
-					-- Generic files
-					"json",
-					"regex",
-					"bash",
-					"gitignore",
-					"markdown",
-					"markdown_inline",
-					"dockerfile",
-					"yaml",
-					"prisma",
-					"dockerfile",
-					"gitignore",
-					"bash",
-					"query",
-
-					-- another lang
-					"go",
-					"rust",
-					"c",
-				},
-			})
-		end,
-	},
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
 }
