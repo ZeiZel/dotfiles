f174a6143a1f8019718673aff974589168471d7a
ZeiZel <valera2003lvov@yandex.ru>
2025-07-01 20:57:43 +0300

    fix: symlinks of nushell

--- symlinks.sh
+++ symlinks.sh
@@ -1,5 +1,7 @@
 mkdir -p ~/.config
+mkdir -p "$HOME/Library/Application Support/nushell"
 
 ln -s ~/.dotfiles/zshrc/.zshrc ~/.zshrc
+ln -sf "$HOME/.dotfiles/nushell" "$HOME/Library/Application Support/nushell"
 
 stow .
